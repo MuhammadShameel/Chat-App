b2213af67acfd10a26d7b5992e4c910185997049
MuhammadShameel <mshameelkz@gmail.com>
2024-07-29 02:37:23 +0500

    create layout and setup sidebar in it

---
+++ src/app/components/Layout.tsx
@@ -0,0 +1,17 @@
+import React, { ReactNode } from "react";
+import Sidebar from "./Sidebar";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="flex h-screen">
+      <Sidebar />
+      <div className="flex-grow p-4 bg-gray-100">{children}</div>
+    </div>
+  );
+};
+
+export default Layout;
