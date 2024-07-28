c8dbf3fca1ef1de84fa0fdfac347eaae5daefed2
MuhammadShameel <mshameelkz@gmail.com>
2024-07-29 02:38:29 +0500

    creae freind .tsx

---
+++ src/app/components/Freind.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Friends: React.FC = () => {
+  return (
+    <div>
+      <h1>Friends</h1>
+      {/* Add friends list and other UI elements here */}
+    </div>
+  );
+};
+
+export default Friends;
